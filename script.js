37dcf93e3104905ef865fd2785ec0e99dd71637a
PHIDELIST <phidelisoluoch@gmail.com>
2023-05-14 08:05:50 +0300

    Added browser local storage to sotre reg details

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-
-let users = [];
-
+// Retrieve user data from local storage if available
+let users = JSON.parse(localStorage.getItem("users")) || [];
 
 function addUser(event) {
   event.preventDefault();
-
 
   const name = document.getElementById("name").value;
   const id = document.getElementById("id").value;
   const country = document.getElementById("country").value;
   const language = document.getElementById("language").value;
 
- 
   const user = {
     name,
     id,
@@ -19,38 +16,33 @@ function addUser(event) {
     language
   };
 
-  
   users.push(user);
 
- 
+  // Save updated user data to local storage
+  localStorage.setItem("users", JSON.stringify(users));
+
   document.getElementById("name").value = "";
   document.getElementById("id").value = "";
   document.getElementById("country").value = "";
   document.getElementById("language").value = "";
 
- 
   updateUserTable();
 }
-
 
 function deleteUser(index) {
-
   users.splice(index, 1);
 
- 
+  // Save updated user data to local storage
+  localStorage.setItem("users", JSON.stringify(users));
+
   updateUserTable();
 }
 
-
 function updateUserTable() {
-
   const tableBody = document.getElementById("userTableBody");
-
-
   tableBody.innerHTML = "";
 
-
-  users.forEach(function (user) {
+  users.forEach(function(user) {
     const newRow = document.createElement("tr");
 
     const nameCell = document.createElement("td");
@@ -72,16 +64,19 @@ function updateUserTable() {
     const actionCell = document.createElement("td");
     const deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
-    deleteButton.addEventListener("click", function () {
+    deleteButton.addEventListener("click", function() {
       deleteUser(users.indexOf(user));
     });
     actionCell.appendChild(deleteButton);
     newRow.appendChild(actionCell);
 
-   
     tableBody.appendChild(newRow);
   });
 }
 
-
 document.getElementById("registrationForm").addEventListener("submit", addUser);
+
+// Update the user table on page load
+window.addEventListener("load", function() {
+  updateUserTable();
+});
